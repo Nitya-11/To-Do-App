ef2213046a68eea7f119e31a8275dec6515aaa28
Nitya-11 <NITYASINGH559@GMAIL.COM>
2025-07-06 12:30:53 +0530

    Add code for the task and also add the delete button

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let inp = document.querySelector("input");
 btn.addEventListener("click",function(){
     let item = document.createElement("li");
     item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+
     ul.appendChild(item);
     inp.value = "";
 })
